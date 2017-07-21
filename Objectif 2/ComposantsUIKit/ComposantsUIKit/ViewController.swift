1d8171495e6d3d72104a89933ff0f988155d5a4c
Maxime Britto <maxbritto@users.noreply.github.com>
2017-07-22 00:15:01 +0200

    Délégation et champ texte

--- Objectif 2/ComposantsUIKit/ComposantsUIKit/ViewController.swift
+++ Objectif 2/ComposantsUIKit/ComposantsUIKit/ViewController.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var ui_emailField: UITextField!
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("L'edition a commencé")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("La personne a appuyé sur return")
+        textField.resignFirstResponder()
+        return false
+    }
+    
     @IBAction func directionDidChange(_ sender: UISegmentedControl) {
         print("Nouvelle direction selectionnée : \(sender.selectedSegmentIndex)")
-        print(sender.titleForSegment(at: sender.selectedSegmentIndex))
+        print(sender.titleForSegment(at: sender.selectedSegmentIndex)!)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        ui_emailField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
