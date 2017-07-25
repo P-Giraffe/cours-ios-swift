1153207aaf15486485669fd01fbca1b4985c3818
Maxime Britto <maxbritto@users.noreply.github.com>
2017-07-25 16:35:13 +0200

    254	Connexion des outlets et actions

--- Objectif 2/Convertisseur/Convertisseur/ViewController.swift
+++ Objectif 2/Convertisseur/Convertisseur/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ui_inputValueType: UISegmentedControl!
+    @IBOutlet weak var ui_inputValueField: UITextField!
+    
+    
+    @IBOutlet weak var ui_outputMeterLabel: UILabel!
+    @IBOutlet weak var ui_outputCentimeterLabel: UILabel!
+    @IBOutlet weak var ui_outputInchesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func inputValueTypeChanged() {
+    }
+    @IBAction func inputValueChanged() {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
