cad595a9fef3f291f285b8ad63f51f1520b51dc0
Maxime Britto <maxbritto@users.noreply.github.com>
2017-07-28 16:20:30 +0200

    333 − UITableViewDelegate

--- Objectif 3/Objectif3_UIKit/Objectif3_UIKit/ViewController.swift
+++ Objectif 3/Objectif3_UIKit/Objectif3_UIKit/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 
+    @IBOutlet weak var ui_tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ui_tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
