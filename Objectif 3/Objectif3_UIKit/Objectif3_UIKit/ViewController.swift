f28fbba1c80336db9d359153b38521be7fe84b6a
Maxime Britto <maxbritto@users.noreply.github.com>
2017-07-28 19:09:45 +0200

    336_Gerer_les_sections

--- Objectif 3/Objectif3_UIKit/Objectif3_UIKit/ViewController.swift
+++ Objectif 3/Objectif3_UIKit/Objectif3_UIKit/ViewController.swift
@@ -9,34 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 500
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "number-cell", for: indexPath)
-        
-        if let titleLabel = cell.textLabel {
-            titleLabel.text = "Cellule \(indexPath.row)"
-        }
-        
-        return cell
-    }
-    
 
     @IBOutlet weak var ui_tableView: UITableView!
+    var _70sShowList:[String] = []
+    var _friendsList:[String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         ui_tableView.dataSource = self
+        _70sShowList.append("Donna")
+        _70sShowList.append("Eric")
+        _70sShowList.append("Kelso")
+        _70sShowList.append("Jackie")
+        _70sShowList.append("Fez")
+        _70sShowList.append("Hide")
+        _70sShowList.append("Bob")
+        _70sShowList.append("Midge")
+        
+        _friendsList.append("Monica")
+        _friendsList.append("Rachel")
+        _friendsList.append("Phoebe")
+        _friendsList.append("Chandler")
+        _friendsList.append("Joey")
+        _friendsList.append("Ross")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let title:String
+        if section == 0 {
+            title = "That 70's Show"
+        } else {
+            title = "Friends"
+        }
+        return title
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let rowCount:Int
+        if section == 0 {
+            rowCount = _70sShowList.count
+        } else {
+            rowCount = _friendsList.count
+        }
+        return rowCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "number-cell", for: indexPath)
+        
+        if let titleLabel = cell.textLabel {
+            if indexPath.section == 0 {
+                titleLabel.text = _70sShowList[indexPath.row]
+            } else {
+                titleLabel.text = _friendsList[indexPath.row]
+            }
+            
+        }
+        
+        return cell
+    }
 }
 
