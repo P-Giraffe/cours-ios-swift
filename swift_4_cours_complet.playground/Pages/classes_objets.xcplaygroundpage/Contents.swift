9aa1ba693dcbdbe173d637960dd4ece957fe4d40
Maxime Britto <maxbritto@users.noreply.github.com>
2017-07-31 18:10:41 +0200

    325_Initialisation

--- swift_4_cours_complet.playground/Pages/classes_objets.xcplaygroundpage/Contents.swift
+++ swift_4_cours_complet.playground/Pages/classes_objets.xcplaygroundpage/Contents.swift
@@ -5,37 +5,53 @@
  ### Maxime Britto - Swift 4
  */
 class Player {
-    var name:String = ""
-    var score:Int = 0
-    var level:Int = 1
+    var name:String
+    private var score:Int = 0
+    private var level:Int = 1
+    
+    init(startName:String) {
+        name = startName
+    }
+    
+    func getScore() -> Int {
+        return score
+    }
+    
+    func getLevel() -> Int {
+        return level
+    }
     
     func gameWon() {
-        score = score + 5
-        updateLevel()
+        changeScore(newScore: score + 5)
     }
     
     func gameLost() {
-        score = score - 2
+        changeScore(newScore: score - 2)
+    }
+    
+    func changeScore(newScore:Int) {
+        score = newScore
         updateLevel()
     }
     
-    func updateLevel() {
+    private func updateLevel() {
         if score < 10 {
             level = 1
-        } else {
+        } else if score < 20 {
             level = 2
+        } else {
+            level = 3
         }
     }
 }
 
-let player1 = Player()
-player1.name = "Sheldon"
-let player2 = Player()
-player2.name = "Leonard"
+let player1 = Player(startName: "Sheldon")
+let player2 = Player(startName: "Leonard")
 player1.gameWon()
 player1.gameWon()
 player2.gameWon()
 player1.gameLost()
-
+player1.changeScore(newScore: 30)
+let player3 = Player(startName: "Penny")
 
 //: [Optionnels >](@next)
