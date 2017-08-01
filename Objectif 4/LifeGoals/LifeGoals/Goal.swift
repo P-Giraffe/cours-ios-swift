7d2b3ecb41f97d0a76f3ffe30b27ce324106dc28
Maxime Britto <maxbritto@users.noreply.github.com>
2017-08-01 17:54:37 +0200

    452_Creation_de_la_classe_Goal

---
+++ Objectif 4/LifeGoals/LifeGoals/Goal.swift
@@ -0,0 +1,18 @@
+//
+//  Goal.swift
+//  LifeGoals
+//
+//  Created by Maxime Britto on 01/08/2017.
+//  Copyright © 2017 Purple Giraffe. All rights reserved.
+//
+
+import Foundation
+
+class Goal {
+    let title:String
+    let category:String? = nil
+    
+    init(newTitle:String) {
+        title = newTitle
+    }
+}
