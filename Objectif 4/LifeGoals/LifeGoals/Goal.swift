dc0466a7e6d2f33de76e99d3a9e1ffe0e0d643e9
Maxime Britto <maxbritto@users.noreply.github.com>
2017-08-01 18:24:45 +0200

    455_Analyse_approfondie_du_langage

--- Objectif 4/LifeGoals/LifeGoals/Goal.swift
+++ Objectif 4/LifeGoals/LifeGoals/Goal.swift
@@ -18,15 +18,19 @@ class Goal {
     }
     
     private func processGoal() {
-        let tagger = NSLinguisticTagger(tagSchemes: [.lexicalClass], options: 0)
+        let tagger = NSLinguisticTagger(tagSchemes: [.lexicalClass, .lemma], options: 0)
         tagger.string = title
         let range = NSRange(location: 0, length: title.count)
         
         tagger.enumerateTags(in: range, scheme: .lexicalClass, options: [.omitPunctuation, .omitWhitespace]) { (tagOpt, tokenRange, _, _) in
             if let tag:NSLinguisticTag = tagOpt,
                 tag == .verb {
-                let verb = (title as NSString).substring(with: tokenRange)
-                category = verb
+                if let lemmaTag = tagger.tag(at: tokenRange.location, scheme: .lemma, tokenRange: nil, sentenceRange: nil) {
+                    category = lemmaTag.rawValue
+                } else {
+                    let verb = (title as NSString).substring(with: tokenRange)
+                    category = verb
+                }
             }
         }
     }
