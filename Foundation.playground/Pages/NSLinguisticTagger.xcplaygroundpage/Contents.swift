1287e87703f2cbb18713ae76abdc057fc105ba13
Maxime Britto <maxbritto@users.noreply.github.com>
2017-08-01 17:46:45 +0200

    444_NSLinguisticTagger_partie_2

--- Foundation.playground/Pages/NSLinguisticTagger.xcplaygroundpage/Contents.swift
+++ Foundation.playground/Pages/NSLinguisticTagger.xcplaygroundpage/Contents.swift
@@ -10,17 +10,20 @@ import Foundation
 
 var text = "Je marchais dans les rues de Paris, quand j'ai rencontré Elon Musk."
 
-let tagger = NSLinguisticTagger(tagSchemes: [.nameTypeOrLexicalClass], options: 0)
+let tagger = NSLinguisticTagger(tagSchemes: [.lexicalClass, .lemma], options: 0)
 tagger.string = text
 
 let range = NSRange(location: 0, length: text.count)
 let options: NSLinguisticTagger.Options = [.omitWhitespace, .omitPunctuation, .joinNames]
 
-tagger.enumerateTags(in: range, scheme: .nameTypeOrLexicalClass, options: options, using:{
+tagger.enumerateTags(in: range, scheme: .lexicalClass, options: options, using:{
     tagOpt, tokenRange, _, _ in
-    if let tag:NSLinguisticTag = tagOpt {
+    if let tag:NSLinguisticTag = tagOpt, tag == .verb {
         let token = (text as NSString).substring(with: tokenRange)
         print("{token: \(token), tag: \(tag.rawValue), range: \(tokenRange)}")
+        if let lemmaTag = tagger.tag(at: tokenRange.location, scheme: .lemma, tokenRange: nil, sentenceRange: nil) {
+            print("Racine : \(lemmaTag.rawValue)")
+        }
     }
 })
 
