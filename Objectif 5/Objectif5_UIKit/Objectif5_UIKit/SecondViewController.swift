f3c7fa244aeb5bdee79579ed08b57f1928bc0775
Maxime Britto <maxbritto@users.noreply.github.com>
2017-09-08 18:25:53 +0200

    531 − Créer un autre écran

---
+++ Objectif 5/Objectif5_UIKit/Objectif5_UIKit/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  Objectif5_UIKit
+//
+//  Created by Maxime Britto on 08/09/2017.
+//  Copyright © 2017 Maxime Britto. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
