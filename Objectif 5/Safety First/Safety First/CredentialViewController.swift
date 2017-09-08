082c309b52156add7f1fcf45ab91f45d52a81f68
Maxime Britto <maxbritto@users.noreply.github.com>
2017-09-08 12:20:55 +0200

    5516 − Outlets et code fonctionnel

--- Objectif 5/Safety First/Safety First/CredentialViewController.swift
+++ Objectif 5/Safety First/Safety First/CredentialViewController.swift
@@ -10,7 +10,35 @@ import UIKit
 import RealmSwift
 
 class CredentialViewController: UIViewController {
-
+    private var _credentials:Credentials?
+    
+    func setCredentials(_ credentials:Credentials) {
+        _credentials = credentials
+    }
+    
+    @IBOutlet weak var ui_loginLabel: UILabel!
+    @IBOutlet weak var ui_passwordLabel: UILabel!
+    @IBOutlet weak var ui_urlLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        displayBasicData()
+    }
+    private func displayBasicData() {
+        if let cred = _credentials {
+            ui_loginLabel.text = "Identifiant : \(cred.login)"
+            ui_urlLabel.text = "Site Web : \(cred.url)"
+        }
+    }
+    
+    @IBAction func displayPassword(_ sender: Any) {
+        if let cred = _credentials {
+            ui_passwordLabel.text = "Mot de passe : \(cred.password)"
+        }
+    }
+    
+    @IBAction func copyPasswordToClipboard(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
