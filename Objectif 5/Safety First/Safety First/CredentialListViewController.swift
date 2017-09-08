07a210fddd5f0b442af90a87054a24575eb8a409
Maxime Britto <maxbritto@users.noreply.github.com>
2017-09-08 11:28:25 +0200

    5513 − Affichage de la liste principale

--- Objectif 5/Safety First/Safety First/CredentialListViewController.swift
+++ Objectif 5/Safety First/Safety First/CredentialListViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class CredentialListViewController: UITableViewController {
-
+    private let _credentialsManager = CredentialsManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -36,24 +37,24 @@ class CredentialListViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return _credentialsManager.getCredentialCount()
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "credential-cell", for: indexPath)
 
-        // Configure the cell...
+        if let credentials = _credentialsManager.getCredential(atIndex: indexPath.row) {
+            cell.textLabel?.text = credentials.title
+        }
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
