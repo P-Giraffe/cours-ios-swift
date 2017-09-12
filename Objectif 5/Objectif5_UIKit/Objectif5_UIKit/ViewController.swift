964d38abe646a3d2c97920d96c543d0247f46dad
Maxime Britto <maxbritto@users.noreply.github.com>
2017-09-12 11:14:35 +0200

    534 − Gérer le presse-papier

--- Objectif 5/Objectif5_UIKit/Objectif5_UIKit/ViewController.swift
+++ Objectif 5/Objectif5_UIKit/Objectif5_UIKit/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var ui_textField: UITextField!
+    @IBOutlet weak var ui_pasteTextButton: UIButton!
+    
+    @IBAction func copyTextToClipboard() {
+        if let text = ui_textField.text {
+            UIPasteboard.general.string = text
+        }
+    }
+    
+    @IBAction func pasteTextFromClipboard() {
+        ui_textField.text = UIPasteboard.general.string
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let pressePapier = UIPasteboard.general
+        ui_pasteTextButton.isHidden = (pressePapier.hasStrings == false)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
