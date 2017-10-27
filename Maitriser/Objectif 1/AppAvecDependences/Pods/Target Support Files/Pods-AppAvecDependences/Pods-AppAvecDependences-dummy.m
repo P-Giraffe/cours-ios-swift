2b1b1fd499d45e3d5481c1bddbff7f0572d090e6
Maxime Britto <maxbritto@users.noreply.github.com>
2017-10-27 10:31:45 +0200

    Découpage du cours en 3 parties
    3 niveaux :
     - niveau 1 : apprendre
     - niveau 2 : maîtriser
     - niveau 3 : avancé

---
+++ Maitriser/Objectif 1/AppAvecDependences/Pods/Target Support Files/Pods-AppAvecDependences/Pods-AppAvecDependences-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppAvecDependences : NSObject
+@end
+@implementation PodsDummy_Pods_AppAvecDependences
+@end
---
+++ Niveau_avance/Objectif 2/Safety First/Pods/Target Support Files/Pods-Safety First/Pods-Safety First-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Safety_First : NSObject
+@end
+@implementation PodsDummy_Pods_Safety_First
+@end
