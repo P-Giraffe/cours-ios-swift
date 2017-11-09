f88b7bdef54265942f86ceca5061bf5ba501bf49
Maxime Britto <maxbritto@users.noreply.github.com>
2017-11-09 15:39:55 +0100

    255_Analyser_un_mot_de_passe_dans_le_SecurityManager

--- Maitriser/Objectif Telechargement/Safety First/Safety First/Model/SecurityManager.swift
+++ Maitriser/Objectif Telechargement/Safety First/Safety First/Model/SecurityManager.swift
@@ -10,6 +10,10 @@ import Foundation
 import Alamofire
 
 class SecurityManager {
+    struct SecurityAnswer {
+        let result:Bool
+        let message:String?
+    }
     private static let TOP_100_PASSWORD_URL = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/10_million_password_list_top_100.txt"
     
     private var _forbiddenPasswordList:[String]? = nil
@@ -37,7 +41,19 @@ class SecurityManager {
         }
     }
     
-    
+    func isPasswordSafe(password:String) -> SecurityAnswer {
+        let answer:SecurityAnswer
+        if let forbiddenPasswordList = _forbiddenPasswordList {
+            if forbiddenPasswordList.contains(password) {
+                answer = SecurityAnswer(result: false, message: "Ce mot de passe fait partie des 100 plus utilisés")
+            } else {
+                answer = SecurityAnswer(result: true, message: nil)
+            }
+        } else {
+            answer = SecurityAnswer(result: false, message: "Le gestionnaire de sécurité n'est pas prêt")
+        }
+        return answer
+    }
     
     
 }
