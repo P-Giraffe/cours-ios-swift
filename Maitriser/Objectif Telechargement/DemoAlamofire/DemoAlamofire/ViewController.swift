72fd7f3064959830833f15e87162a3f03da8c623
Maxime Britto <maxbritto@users.noreply.github.com>
2017-11-10 23:08:46 +0100

    245_Exemple_concret

--- Maitriser/Objectif Telechargement/DemoAlamofire/DemoAlamofire/ViewController.swift
+++ Maitriser/Objectif Telechargement/DemoAlamofire/DemoAlamofire/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ui_sourceTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://www.apple.com").responseString { (response) in
+            if let htmlBody = response.result.value {
+                self.ui_sourceTextView.text = htmlBody
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
