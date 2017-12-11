deaa9a14b6be2b6b6890503ddbf511b54b277592
Maxime Britto <maxbritto@users.noreply.github.com>
2017-12-11 18:06:25 +0100

    7518_Animations_de_transitions

--- Maitriser/Objectif_interface_dynamique/SecretNumber/SecretNumber/ViewController.swift
+++ Maitriser/Objectif_interface_dynamique/SecretNumber/SecretNumber/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var ui_guessedValueField: UITextField!
     @IBOutlet weak var ui_gameStatusLabel: UILabel!
     @IBOutlet weak var ui_lowBoundaryLabel: UILabel!
+    @IBOutlet weak var ui_boundaryZone: UIView!
     
     @IBOutlet weak var ui_checkValueButton: UIButton!
     @IBOutlet weak var cs_boundaryZoneLeading: NSLayoutConstraint! {
@@ -63,16 +64,20 @@ class ViewController: UIViewController {
     
     func updateDisplay() {
         if _gameController.isGameInProgress {
-            ui_gameStatusLabel.text = "Essayez de trouver le nombre mystère"
-            ui_guessedValueField.isHidden = false
-            ui_checkValueButton.isHidden = false
-            
+            if ui_boundaryZone.isHidden != false {
+                UIView.transition(with: ui_boundaryZone, duration: 0.3, options: [.transitionCurlDown], animations: {
+                    self.ui_boundaryZone.isHidden = false
+                }, completion: nil)
+                
+                ui_gameStatusLabel.text = "Essayez de trouver le nombre mystère"
+                ui_guessedValueField.isHidden = false
+                ui_checkValueButton.isHidden = false
+                UIView.animate(withDuration: 0.4, animations: {
+                    self.view.layoutIfNeeded()
+                })
+            }
             ui_lowBoundaryLabel.text = String(_gameController.lowBoundary)
             ui_highBoundaryLabel.text = String(_gameController.highBoundary)
-
-            UIView.animate(withDuration: 0.4, animations: {
-                self.view.layoutIfNeeded()
-            })
 
             cs_boundaryZoneLeading.constant = ViewController.BORDER_MARGIN + CGFloat(_gameController.lowBoundary) * _gameRangeToScreenRatio
             cs_boundaryZoneTrailing.constant = ViewController.BORDER_MARGIN + CGFloat(GameController.MAX_VALUE - _gameController.highBoundary) * _gameRangeToScreenRatio
@@ -85,6 +90,12 @@ class ViewController: UIViewController {
             ui_gameStatusLabel.text = nil
             ui_guessedValueField.isHidden = true
             ui_checkValueButton.isHidden = true
+            if ui_boundaryZone.isHidden != true {
+                UIView.transition(with: ui_boundaryZone, duration: 0.3, options: [.transitionCurlUp], animations: {
+                    self.ui_boundaryZone.isHidden = true
+                }, completion: nil)
+            }
+            
         }
     }
     
