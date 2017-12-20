a3a37954ab9cd59bb464915f531aa5f22bb7f10d
Maxime Britto <maxbritto@users.noreply.github.com>
2017-12-20 17:21:23 +0100

    741_Principes_des_tests_unitaires 742_Configurer_son_projet

---
+++ Maitriser/Objectif_interface_dynamique/DemoTestsUnitaires/DemoTestsUnitaires/Player.swift
@@ -0,0 +1,31 @@
+//
+//  Player.swift
+//  DemoTestsUnitaires
+//
+//  Created by Maxime Britto on 20/12/2017.
+//  Copyright © 2017 Maxime Britto. All rights reserved.
+//
+
+import Foundation
+class Player {
+    private var _score:Int = 0
+    private var _level:Int = 1
+    
+    func getLevel() -> Int {
+        return _level
+    }
+    
+    func changeScore(newScore:Int) {
+        _score = newScore
+    }
+    
+    private func updateLevel() {
+        if _score < 10 {
+            _level = 1
+        } else if _score < 20 {
+            _level = 2
+        } else {
+            _level = 3
+        }
+    }
+}
---
+++ Maitriser/Objectif_interface_dynamique/DemoTestsUnitaires/DemoTestsUnitaires/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoTestsUnitaires
+//
+//  Created by Maxime Britto on 20/12/2017.
+//  Copyright © 2017 Maxime Britto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
