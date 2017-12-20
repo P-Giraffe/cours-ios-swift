647a73884838eaf3e74f03ee2f4f10073aaa1b1c
Maxime Britto <maxbritto@users.noreply.github.com>
2017-12-20 16:38:26 +0100

    738_safeAreaInsets

--- Maitriser/Objectif_interface_dynamique/UIKitAnimation/UIKitAnimation/ViewController.swift
+++ Maitriser/Objectif_interface_dynamique/UIKitAnimation/UIKitAnimation/ViewController.swift
@@ -14,12 +14,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var cs_demoViewLeading: NSLayoutConstraint!
     
     @IBOutlet weak var cs_demoViewHeight: NSLayoutConstraint!
+    var animator:UIViewPropertyAnimator?
+    
     @IBAction func animate() {
+        ui_demoView.frame = CGRect(x: self.view.safeAreaInsets.left, y: self.view.safeAreaInsets.top, width: 150, height: 150)
+        /*
+        //UIPropertyAnimator
+        if animator == nil {
+            animator = UIViewPropertyAnimator(duration: 4, dampingRatio: 0.25) {
+                self.ui_demoView.center = self.view.center
+            }
+            animator!.addAnimations {
+                self.ui_demoView.alpha = 0.5
+            }
+            animator!.startAnimation()
+        } else {
+            if animator!.isRunning {
+                animator!.pauseAnimation()
+            } else {
+                animator!.startAnimation()
+            }
+        }
+        */
+        
+        /*
+        //Transitions
         UIView.transition(with: ui_demoView, duration: 0.75, options: [.transitionCrossDissolve], animations: {
             self.ui_demoView.isHidden = true
         }, completion: nil)
+        */
         
         /*
+         //Ressorts et oscillations
         cs_demoViewHeight.constant = 300
         UIView.animate(withDuration: 1.00, delay: 0, usingSpringWithDamping: 0.35, initialSpringVelocity: 30, options: [], animations: {
             self.view.layoutIfNeeded()
@@ -27,6 +53,7 @@ class ViewController: UIViewController {
         */
         
         /*
+         //Animations simples
         UIView.animate(withDuration: 1.0, delay: 0.5, options: [.curveEaseIn], animations: {
             self.ui_demoView.frame = CGRect(x: 0, y: 0, width: 150, height: 150)
             self.ui_demoView.alpha = 0.25
