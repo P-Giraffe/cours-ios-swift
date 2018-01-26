b6c44757498c260f1906279d6be8e451e8625c51
Maxime Britto <maxbritto@users.noreply.github.com>
2018-01-26 11:40:54 +0100

    842_Creer_un_objet_AVCaptureSession

---
+++ Niveau_avance/Objectif 4/demos/demos/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  demos
+//
+//  Created by Maxime Britto on 26/01/2018.
+//  Copyright © 2018 Maxime Britto. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    @IBOutlet weak var ui_preview: UIView!
+    let captureSession = AVCaptureSession()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func startSessionButtonPressed(_ sender: Any) {
+        configureCaptureSession()
+    }
+    
+    func configureCaptureSession() {
+        // 1 - Configurer les entrées
+        
+        // 2 - Configurer les sorties
+        
+        // 3 - Configurer l'apercu
+        
+        // 4 - Demarrer la session
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
