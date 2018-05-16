ba2c4a874451649907d426efdf29c3cb90f1341a
Maxime Britto <maxbritto@users.noreply.github.com>
2018-05-16 16:54:12 +0200

    924_Preparation_du_projet

---
+++ Niveau_avance/Objectif 5 Synchronisation/Safety First/Safety First/CredentialViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Safety First
+//
+//  Created by Maxime Britto on 05/09/2017.
+//  Copyright © 2017 Maxime Britto. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class CredentialViewController: UIViewController {
+    private var _credentials:Credentials?
+    
+    func setCredentials(_ credentials:Credentials) {
+        _credentials = credentials
+    }
+    
+    @IBOutlet weak var ui_loginLabel: UILabel!
+    @IBOutlet weak var ui_passwordLabel: UILabel!
+    @IBOutlet weak var ui_urlLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        displayBasicData()
+    }
+    private func displayBasicData() {
+        if let cred = _credentials {
+            self.title = cred.title
+            ui_loginLabel.text = "Identifiant : \(cred.login)"
+            ui_urlLabel.text = "Site Web : \(cred.url)"
+        }
+    }
+    
+    @IBAction func displayPassword(_ sender: Any) {
+        if let cred = _credentials {
+            ui_passwordLabel.text = "Mot de passe : \(cred.password)"
+        }
+    }
+    
+    @IBAction func copyPasswordToClipboard(_ sender: Any) {
+        if let cred = _credentials {
+            UIPasteboard.general.string = cred.password
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
